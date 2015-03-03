43ae3b55e3e23241f921bc750e6de570e0f84a51
dmccarey <danmccarey@gmail.com>
2015-03-03 12:55:13 -0500

    Hooking up the devseed parse account

--- source_assets/scripts/main.js
+++ source_assets/scripts/main.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
   'use strict';
 
   L.mapbox.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6IlhHVkZmaW8ifQ.hAMX5hSW-QnTeRCMAy9A8Q';
-  Parse.initialize("VHXflTAD6nz8NAMNLX7NjSfbiSXH0El18vz4pqrN", "vCizo6jB9bkogr4rKRAupHEfNebEls1IbEWHBeJ3");
+  Parse.initialize("8zbh9doCtxZXGe9tOtdJKkhamoOTtYupuT2nk3KQ", "yYCLA97XA6xDh5FyoKo3a2Yoaf7L4y6bAWxJL5V0");
 
   Grp.init();
 });
